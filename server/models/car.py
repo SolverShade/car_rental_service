ac3732394c5292bff02dd7fc21cac5ceb82f608d
solvershade <seconduacc07@gmail.com>
2024-10-31 11:04:51 -0400

    added picture url field to car model

--- server/models/car.py
+++ server/models/car.py
@@ -12,10 +12,12 @@ class Car(db.Model):
     model = db.Column(db.String, nullable=False)
     daily_cost = db.Column(Numeric(10, 2), nullable=False)
     rented = db.Column(db.Boolean, nullable=False)
+    image_url = db.Column(db.String, nullable=False)
 
-    def __init__(self, name, make, model, daily_cost, rented):
+    def __init__(self, name, make, model, daily_cost, rented, image_url):
         self.name = name
         self.make = make
         self.model = model
         self.daily_cost = daily_cost
         self.rented = rented
+        self.image_url = image_url
